95f8891d91f4624b27b519a061ca8ad2f07aa151
moroz_olexandr <oleksandrmoroz2003@gmail.com>
2023-10-31 00:09:57 +0200

    finish user app

--- user/views.py
+++ user/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView, RetrieveAPIView
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from user.serializers import UserSerializer
+
+
+class UserCreateView(CreateAPIView):
+    serializer_class = UserSerializer
+
+
+class UserDetailView(RetrieveAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
