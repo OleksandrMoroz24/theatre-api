a3465073d1a2c0414d05031a6942051e52e5397a
moroz_olexandr <oleksandrmoroz2003@gmail.com>
2023-11-06 20:55:28 +0200

    add postgresql settings and fix imports

--- theatre/tests/test_serializers.py
+++ theatre/tests/test_serializers.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+
 from theatre.models import Actor
 from theatre.serializers import ActorSerializer
 
--- theatre/tests/test_views.py
+++ theatre/tests/test_views.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 from rest_framework import status
 from rest_framework.test import APITestCase, APIClient
 from rest_framework.reverse import reverse
+
 from theatre.models import Actor, Genre, Play
 
 
